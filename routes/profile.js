0316d1eb547f6f77deb66deac8be8302e4e2a3a3
DarielMorua <12119l@ulsachihuahua.edu.mx>
2024-10-15 19:19:54 -0600

    Inicial Commit DEV

--- routes/profile.js
+++ routes/profile.js
@@ -1,14 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.render('profile');
-    });
-router.put('/', function(req, res, next) {
-    res.render('profile');
-    }   );
-router.delete('/', function(req, res, next) {
-    res.render('profile');
-    }
-    );
+router.get("/", function (req, res, next) {
+  res.render("profile");
+});
+router.put("/", function (req, res, next) {
+  res.render("profile");
+});
+router.delete("/", function (req, res, next) {
+  res.render("profile");
+});
+
 module.exports = router;
