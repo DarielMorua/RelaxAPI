1ab526c1922efea4742f7e8ac9b7aa4b68a602d5
DarielMorua <12119l@ulsachihuahua.edu.mx>
2024-10-18 18:07:52 -0600

    Se arregla un error al hacer mal merge con professionals agregando professional router

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-var profesionalRouter = require('./routes/professionals');
+var profesionalRouter = require("./routes/professionals");
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -12,6 +11,7 @@ const mongo = process.env.MONGO_DB;
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var profesionalRouter = require("./routes/professionals");
 
 var app = express();
 
@@ -37,9 +37,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-
-app.use('/professionals', profesionalRouter);
-
+app.use("/professionals", profesionalRouter);
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
@@ -48,7 +46,6 @@ app.use("/users", usersRouter);
 app.use(function (req, res, next) {
   next(createError(404));
 });
-
 
 // error handler
 app.use(function (err, req, res, next) {
@@ -60,6 +57,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
 
 module.exports = app;
