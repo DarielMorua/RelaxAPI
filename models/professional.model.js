023327a607997abcfccf87adc7db9d1e4457c195
JuveyGaymer XD <juvey746@gmail.com>
2024-10-22 21:17:24 -0700

    Se crea servicio para autentificar una contraseña de un usuario

--- models/professional.model.js
+++ models/professional.model.js
@@ -52,10 +52,6 @@ const profesionalSchema = new mongoose.Schema({
     type: Boolean,
     default: true,
   },
-  isFavorite: {
-    type: Boolean,
-    default: false,
-  },
 });
 
 // Crear el modelo basado en el esquema
